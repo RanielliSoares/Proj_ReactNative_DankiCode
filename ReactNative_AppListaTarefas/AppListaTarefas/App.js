f81f3e6c41ac5f03d9c00d7a53a95c6c86800a69
Ranielli Soares <ranielli.pinho@outlook.com>
2024-01-31 00:38:11 -0300

    Update Header

--- ReactNative_AppListaTarefas/AppListaTarefas/App.js
+++ ReactNative_AppListaTarefas/AppListaTarefas/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
+
+  const image = require('./resources/img/bg.jpg')
+
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo App Tarefas!</Text>
-      <StatusBar style="auto" />
+
+      <ImageBackground source={image} style={styles.img}>
+        <View style={styles.coverView}>
+          <Text style={styles.textHeader}>Lista de Tarefas - Danki Code</Text>
+        </View>
+      </ImageBackground>
+
     </View>
   );
 }
@@ -13,8 +21,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  img: {
+    width: '100%',
+    height: 80,
+    resizeMode: 'cover',
+  },
+  coverView: {
+    width: '100%',
+    height: 80,
+    backgroundColor: 'rgba(0,0,0,0.6)'
+  },
+  textHeader: {
+    textAlign:'center',
+    color:'#FFF',
+    fontSize:20,
+    marginTop:20
+  }
 });
